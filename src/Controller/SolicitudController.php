c59fd7c9b691525a1447966c40dbe1e586c3861f
Andrés <andres.plandolit@gmail.com>
2021-10-13 08:58:52 -0300

    FlashAlerts

--- src/Controller/SolicitudController.php
+++ src/Controller/SolicitudController.php
@@ -27,38 +27,37 @@ class SolicitudController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            //TODO: Verificar el CUIT y/o el CUIL que sean correctos y válidos (Preguntar a Gustavo del algoritmo de verificación)
-            //TODO: Verificar que el email y/o CUIT/CUIL no estén repetidos
-
+            $entityManager = $this->getDoctrine()->getManager();
+            
+            //Verifica si ya existe una solicitud con el Cuit, Cuil o Mail
+            if ($this->verificarSolicitudPreexistente($solicitud) == true){
+                return $this->redirectToRoute('dashboard');
+            }
+            
             //TODO: Este no es el hash solicitado por la documentación: (openssl_encrypt, cifrado 'AES-256-CBC')
             $hash = md5(uniqid(rand(), true));
             $solicitud->setHash($hash);
 
-            $entityManager = $this->getDoctrine()->getManager();
+            
             $entityManager->persist($solicitud);
             $entityManager->flush();
 
+            //TODO: Verificar que el email se haya enviado sin errores
             $url = $this->generateUrl('solicitud-paso-2', ['hash' => $hash], UrlGeneratorInterface::ABSOLUTE_URL);
             $email = (new TemplatedEmail())
             ->from('hello@example.com')
             ->to($solicitud->getMail())
-            //->cc('cc@example.com')
-            //->bcc('bcc@example.com')
-            //->replyTo('fabien@example.com')
-            //->priority(Email::PRIORITY_HIGH)
             ->subject('Invitación para dar de alta usuario y dispositivo nuevo')            
             ->htmlTemplate('emails/invitacionPasoUno.html.twig')
             ->context([
                 'nicname' => $solicitud->getNicname(),
                 'url' => $url
             ])
-            //->text('Hola ' . $solicitud->getNicname() . '! Por favor, ingrese a este link ' . $url . ' para completar su solicitud')
-            //->html('<p>See Twig integration for better HTML integration!</p>')
             ;
 
             $mailer->send($email);
 
-            //TODO: Agregar un flashMessage y mostrarlo en el dashboard para hacerlo amigable al usuario
+            $this->addFlash('success', 'Invitación creada con éxito!');
             return $this->redirectToRoute('dashboard');
         }
 
@@ -96,7 +95,9 @@ class SolicitudController extends AbstractController
             $entityManager->persist($solicitud);
             $entityManager->persist($dispositivo);
             $entityManager->flush();
-            //TODO: agregar el flashbag;
+            
+            $this->addFlash('success', 'Datos completados con éxito!');
+
             //TODO: Que el redirectToRoute vaya al home cuando el mismo esté listo
             return $this->redirectToRoute('dashboard');
         }
@@ -110,8 +111,34 @@ class SolicitudController extends AbstractController
     /**
      * @Route("dashboard/{hash}/generar-usuario", name="generarNuevoUsuario")
      */
-    public function pasoTres(Request $request): Response
+    public function pasoTresUno(Request $request): Response
     {
         return $this->render('$0.html.twig', []);
+    }
+
+    private function verificarSolicitudPreexistente($solicitud){
+        $entityManager = $this->getDoctrine()->getManager();
+        //TODO: Verificar el CUIT y/o el CUIL que sean correctos y válidos (Preguntar a Gustavo del algoritmo de verificación)
+        $verificarMailExistente = $entityManager->getRepository('App\Entity\Solicitud')->findOneByMail($solicitud->getMail());
+        $verificarCuilExistente = $entityManager->getRepository('App\Entity\Solicitud')->findOneByCuil($solicitud->getCuil());
+        $verificarCuitExistenet = $entityManager->getRepository('App\Entity\Solicitud')->findOneByCuit($solicitud->getCuit());
+
+        if ($verificarCuilExistente){
+            $this->addFlash('danger', 'Existe una solicitud con ese CUIT');
+        }
+
+        if ($verificarCuitExistenet){
+            $this->addFlash('danger', 'Existe una solicitud con ese CUIL');
+        }
+
+        if ($verificarMailExistente) {
+            $this->addFlash('danger', 'Existe una solicitud con esa dirección de Email');
+        }
+
+        if ($verificarMailExistente || $verificarCuilExistente || $verificarCuitExistenet){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
