004010e533a3e9399efe3b1760dbcad778ca3828
emigimenezj <emigimenezj@zohomail.com>
2021-11-23 15:05:38 -0300

    Validación interactiva en tiempo real para nueva solicitud terminada

--- public/js/validatorform.js
+++ public/js/validatorform.js
@@ -3,7 +3,7 @@ function validarForm(nameform){
     req.overrideMimeType("application/json");
     req.open('GET', "/datavalidator/"+nameform, true);
     req.onload  = function() {
-       var jsonResponse = JSON.parse(req.responseText);
+       let jsonResponse = JSON.parse(req.responseText);
        // alert(JSON.stringify(jsonResponse))
        aplicarReglas(jsonResponse)
     };
@@ -12,23 +12,25 @@ function validarForm(nameform){
 
 function aplicarReglas(js)
 {
-    for (var key in js){
+    for (let key in js){
         procesarComponente(js[key])
     }
 }
 
 function procesarComponente(obj){
-    id = obj['fieldname']
-    var element = document.getElementById(id);
+    let id = obj['fieldname'];
+    let id_err = obj['fieldname_err'];
+    let element = document.getElementById(id);
+    let element_err = document.getElementById(id_err);
     if(element != null)
     {
-        validadores(element, obj )
+        validadores(element, obj, element_err)
     }else{
         alert('No existe el campo: '+obj['fieldname'])
     }
 }
 
-function validadores(element, obj ){
+function validadores(element, obj, element_err){
     //--- coloca placeholder ---
     if(obj['val_placeholder']){
         element.placeholder=obj['val_placeholder']
@@ -36,19 +38,50 @@ function validadores(element, obj ){
 
     if(obj['is_cuit'])
     {
-        element.addEventListener('input', () => {
+        element.addEventListener('keydown', event => {
+            if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') return
+            element.selectionStart = element.textLength;
+            element.selectionEnd = element.textLength;
+            element.value = element.value[1] && element.value[2]!=='-' ? element.value.slice(0,2) + '-' + element.value.slice(2) : element.value;
+            element.value = element.value[10] && element.value[11]!=='-' ? element.value.slice(0,11) + '-' + element.value.slice(11) : element.value;
+            if (event.key === 'Backspace' && element.selectionStart === 3 && element.selectionEnd === 3 && element.textLength === 3) {
+                event.preventDefault();
+                element.value = element.value.slice(0, -2);
+                return
+            }
+            if (event.key === 'Backspace' && element.selectionStart === 12 && element.selectionEnd === 12 && element.textLength === 12) {
+                event.preventDefault();
+                element.value = element.value.slice(0, -2);
+            }
+        });
+
+        element.addEventListener('keypress', event => {
+            if (!validator.isNumeric(event.key) || element.textLength >= 13) event.preventDefault()
+        });
+
+        element.addEventListener('focusout', event => {
             element.setCustomValidity("");
-            if(!isValidCUITCUIL(element.value))
-                element.setCustomValidity(obj['is_cuit_error'])
+            if(!isValidCUITCUIL(element.value)) {
+                element.setCustomValidity(obj['is_cuit_error']);
+                element_err.innerHTML = obj['is_cuit_error'];
+                element_err.style.color = 'red'; // TODO es provisorio, habría que sacarlo y hacerlo desde CSS
+            } else {
+                element_err.innerHTML = '';
+            }
         });
     }
 
     if(obj['is_email'])
     {
-        element.addEventListener('input', () => {
+        element.addEventListener('focusout', () => {
             element.setCustomValidity("");
-            if (!validator.isEmail(element.value))
-                element.setCustomValidity(obj['is_email_error'])
+            if (!validator.isEmail(element.value)) {
+                element.setCustomValidity(obj['is_email_error']);
+                element_err.innerHTML = obj['is_email_error'];
+                element_err.style.color = 'red'; // TODO es provisorio, habría que sacarlo y hacerlo desde CSS
+            } else {
+                element_err.innerHTML = '';
+            }
         });
     }
 
@@ -56,13 +89,18 @@ function validadores(element, obj ){
     if(obj['is_emailrepeat'])
     {
         params = JSON.parse(obj['extraproperties'])
-        var otroelement = document.getElementById(params['email']);
+        let otroelement = document.getElementById(params['email']);
         if(otroelement != null)
         {
-            element.addEventListener('input', () => {
+            element.addEventListener('focusout', () => {
                 element.setCustomValidity("");
-                if (element.value !== otroelement.value)
-                  element.setCustomValidity(obj['is_emailrepeat_error'])
+                if (element.value !== otroelement.value) {
+                    element.setCustomValidity(obj['is_emailrepeat_error']);
+                    element_err.innerHTML = obj['is_emailrepeat_error'];
+                    element_err.style.color = 'red'; // TODO es provisorio, habría que sacarlo y hacerlo desde CSS
+                } else {
+                    element_err.innerHTML = '';
+                }
             });
         }else{
             alert("Validacion: No existe campo emailrepeat:"+params['email']+" en extraproperties\n- Posiblemente debera anteponer el nombre del formulario: nombreform_"+params['email']+"\n- Verifique ademas el nombre")
